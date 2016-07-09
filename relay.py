7956252b583dfca04f716113554e05bfef819432
Richard Deurwaarder (Xeli) <richard@xeli.eu>
2016-07-09 19:49:19 +0000

    add a comment to clarify why the relay array starts with -1

--- relay.py
+++ relay.py
@@ -1,5 +1,7 @@
 import RPi.GPIO as GPIO
 
+# the -1 is there to make this 1-based
+# because the relay id's on the board start at 1
 relays=[-1, 7, 8, 25, 24, 23, 18, 15, 4]
 
 def setupRelayOUT(relayNumber):
