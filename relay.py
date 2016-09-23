160de571bfe566528010bce4287d1cb0b0966be8
Richard Deurwaarder (Xeli) <richard@xeli.eu>
2016-09-23 18:24:15 +0000

    new relay layout, will probably change soon with rpi 3

--- relay.py
+++ relay.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 
 # the -1 is there to make this 1-based
 # because the relay id's on the board start at 1
-relays=[-1, 7, 8, 25, 24, 23, 18, 15, 4]
+relays=[-1, 7, -1, -1, 24, 23, 18, 15, 4]
 
 def setupRelayOUT(relayNumber):
     GPIO.setup(relays[relayNumber], GPIO.OUT)
